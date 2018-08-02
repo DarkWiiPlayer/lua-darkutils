a2a772027837f226d8777589d93c227d0cf91b6f
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2018-08-02 11:26:42 +0200

    Fix minor error in init file

--- darkutils/init.lua
+++ darkutils/init.lua
@@ -5,7 +5,7 @@ darkutils = {
 
 return setmetatable(darkutils, {__call=function(self, override)
   local global = _ENV or _G
-  for name, submodule in pairs(darkutils) do
+  for name, submodule in pairs(self) do
     local module = global[name] or {}
     for key, value in pairs(submodule) do
       module[key] = not override and module[key] or value
